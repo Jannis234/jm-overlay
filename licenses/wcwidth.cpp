fde02f13d66d54e080a8339f472017565cdc487f
Jannis Mast <>
2016-02-17 23:29:23 +0100

    Added dev-libs/linenoise-ng

---
+++ licenses/wcwidth.cpp
@@ -0,0 +1,5 @@
+Markus Kuhn -- 2007-05-26 (Unicode 5.0)
+
+Permission to use, copy, modify, and distribute this software
+for any purpose and without fee is hereby granted. The author
+disclaims all warranties with regard to this software.
