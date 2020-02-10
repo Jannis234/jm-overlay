c20a143c38e3a8785031b38af02a233f09b57c51
Jannis Mast <jannis@ctrl-c.xyz>
2020-02-10 23:32:24 +0100

    Clean up licenses

--- licenses/wcwidth.cpp
+++
@@ -1,5 +0,0 @@
-Markus Kuhn -- 2007-05-26 (Unicode 5.0)
-
-Permission to use, copy, modify, and distribute this software
-for any purpose and without fee is hereby granted. The author
-disclaims all warranties with regard to this software.
